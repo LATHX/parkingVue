3392c04455f6b4765364ec804b1f4ea8de20caa3
RATHX <ljl@stu.jluzh.edu.cn>
2025-07-11 10:22:33 +0800

    init

--- src/views/parking/merchant/ParkingMerchant.data.ts
+++ src/views/parking/merchant/ParkingMerchant.data.ts
@@ -1,42 +1,41 @@
-import {BasicColumn} from '/@/components/Table';
-import {FormSchema} from '/@/components/Table';
-import { rules} from '/@/utils/helper/validator';
+import { BasicColumn } from '/@/components/Table';
+import { FormSchema } from '/@/components/Table';
+import { rules } from '/@/utils/helper/validator';
 import { render } from '/@/utils/common/renderUtils';
 import { getWeekMonthQuarterYear } from '/@/utils';
 //列表数据
 export const columns: BasicColumn[] = [
   {
-    title: '微信绑定openId',
-    align: "center",
-    dataIndex: 'openId'
+    title: '名称',
+    align: 'center',
+    dataIndex: 'merchantName',
   },
   {
-    title: '微信绑定unionId',
-    align: "center",
-    dataIndex: 'unionId'
+    title: '手机号',
+    align: 'center',
+    dataIndex: 'phone',
   },
   {
     title: '城市',
-    align: "center",
-    dataIndex: 'city'
+    align: 'center',
+    dataIndex: 'city',
   },
   {
     title: '是否可用',
-    align: "center",
-    dataIndex: 'status'
+    align: 'center',
+    dataIndex: 'status',
   },
   {
     title: '父账号',
-    align: "center",
-    dataIndex: 'parentId'
+    align: 'center',
+    dataIndex: 'parentId',
   },
 ];
 
 // 高级查询数据
 export const superQuerySchema = {
-  openId: {title: '微信绑定openId',order: 0,view: 'text', type: 'string',},
-  unionId: {title: '微信绑定unionId',order: 1,view: 'text', type: 'string',},
-  city: {title: '城市',order: 2,view: 'text', type: 'string',},
-  status: {title: '是否可用',order: 3,view: 'text', type: 'string',},
-  parentId: {title: '父账号',order: 4,view: 'text', type: 'string',},
+  merchantName: { title: '名称', order: 0, view: 'text', type: 'string' },
+  phone: { title: '手机号', order: 1, view: 'text', type: 'string' },
+  city: { title: '城市', order: 2, view: 'sel_search', type: 'string', dictCode: 'city' },
+  parentId: { title: '父账号', order: 4, view: 'sel_search', type: 'string', dictCode: 'parking_merchant,phone,id' },
 };
--- src/views/parking/merchant/components/ParkingMerchantForm.vue
+++ src/views/parking/merchant/components/ParkingMerchantForm.vue
@@ -4,31 +4,42 @@
       <template #detail>
         <a-form ref="formRef" class="antd-modal-form" :labelCol="labelCol" :wrapperCol="wrapperCol" name="ParkingMerchantForm">
           <a-row>
-						<a-col :span="24">
-							<a-form-item label="微信绑定openId" v-bind="validateInfos.openId" id="ParkingMerchantForm-openId" name="openId">
-								<a-input v-model:value="formData.openId" placeholder="请输入微信绑定openId"  allow-clear ></a-input>
-							</a-form-item>
-						</a-col>
-						<a-col :span="24">
-							<a-form-item label="微信绑定unionId" v-bind="validateInfos.unionId" id="ParkingMerchantForm-unionId" name="unionId">
-								<a-input v-model:value="formData.unionId" placeholder="请输入微信绑定unionId"  allow-clear ></a-input>
-							</a-form-item>
-						</a-col>
-						<a-col :span="24">
-							<a-form-item label="城市" v-bind="validateInfos.city" id="ParkingMerchantForm-city" name="city">
-								<a-input v-model:value="formData.city" placeholder="请输入城市"  allow-clear ></a-input>
-							</a-form-item>
-						</a-col>
-						<a-col :span="24">
-							<a-form-item label="是否可用" v-bind="validateInfos.status" id="ParkingMerchantForm-status" name="status">
-								<a-input v-model:value="formData.status" placeholder="请输入是否可用"  allow-clear ></a-input>
-							</a-form-item>
-						</a-col>
-						<a-col :span="24">
-							<a-form-item label="父账号" v-bind="validateInfos.parentId" id="ParkingMerchantForm-parentId" name="parentId">
-								<a-input v-model:value="formData.parentId" placeholder="请输入父账号"  allow-clear ></a-input>
-							</a-form-item>
-						</a-col>
+            <a-col :span="24">
+              <a-form-item label="名称" v-bind="validateInfos.merchantName" id="ParkingMerchantForm-merchantName" name="merchantName">
+                <a-input v-model:value="formData.merchantName" placeholder="请输入名称" allow-clear></a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :span="24">
+              <a-form-item label="手机号" v-bind="validateInfos.phone" id="ParkingMerchantForm-phone" name="phone">
+                <a-input v-model:value="formData.phone" placeholder="请输入手机号" allow-clear></a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :span="24">
+              <a-form-item label="微信绑定openId" v-bind="validateInfos.openId" id="ParkingMerchantForm-openId" name="openId">
+                <a-input v-model:value="formData.openId" placeholder="请输入微信绑定openId" allow-clear></a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :span="24">
+              <a-form-item label="微信绑定unionId" v-bind="validateInfos.unionId" id="ParkingMerchantForm-unionId" name="unionId">
+                <a-input v-model:value="formData.unionId" placeholder="请输入微信绑定unionId" allow-clear></a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :span="24">
+              <a-form-item label="城市" v-bind="validateInfos.city" id="ParkingMerchantForm-city" name="city">
+                <JSearchSelect v-model:value="formData.city" dictCode="city" placeholder="请输入城市" allow-clear />
+
+              </a-form-item>
+            </a-col>
+            <a-col :span="24">
+              <a-form-item label="父账号" v-bind="validateInfos.parentId" id="ParkingMerchantForm-parentId" name="parentId">
+                <j-search-select v-model:value="formData.parentId" dictCode="parking_merchant,phone,id" placeholder="请选择父账号" allow-clear />
+              </a-form-item>
+            </a-col>
+            <a-col :span="24">
+              <a-form-item label="是否可用" v-bind="validateInfos.status" id="ParkingCustomerForm-status" name="status">
+                <j-dict-select-tag v-model:value="formData.status" dictCode="yn" placeholder="请输入是否可用" allow-clear />
+              </a-form-item>
+            </a-col>
           </a-row>
         </a-form>
       </template>
@@ -44,21 +55,26 @@
   import { saveOrUpdate } from '../ParkingMerchant.api';
   import { Form } from 'ant-design-vue';
   import JFormContainer from '/@/components/Form/src/container/JFormContainer.vue';
+  import JDictSelectTag from '../../../../components/Form/src/jeecg/components/JDictSelectTag.vue';
+  import { JSearchSelect } from '@/components/Form';
+
   const props = defineProps({
     formDisabled: { type: Boolean, default: false },
-    formData: { type: Object, default: () => ({})},
-    formBpm: { type: Boolean, default: true }
+    formData: { type: Object, default: () => ({}) },
+    formBpm: { type: Boolean, default: true },
   });
   const formRef = ref();
   const useForm = Form.useForm;
   const emit = defineEmits(['register', 'ok']);
   const formData = reactive<Record<string, any>>({
     id: '',
-    openId: '',   
-    unionId: '',   
-    city: '',   
-    status: '',   
-    parentId: '',   
+    phone: '',
+    merchantName: '',
+    openId: '',
+    unionId: '',
+    city: '',
+    status: '',
+    parentId: '',
   });
   const { createMessage } = useMessage();
   const labelCol = ref<any>({ xs: { span: 24 }, sm: { span: 5 } });
@@ -66,23 +82,22 @@
   const confirmLoading = ref<boolean>(false);
   //表单验证
   const validatorRules = reactive({
-    status: [{ required: true, message: '请输入是否可用!'},],
+    status: [{ required: true, message: '请输入是否可用!' }],
   });
   const { resetFields, validate, validateInfos } = useForm(formData, validatorRules, { immediate: false });
 
   // 表单禁用
-  const disabled = computed(()=>{
-    if(props.formBpm === true){
-      if(props.formData.disabled === false){
+  const disabled = computed(() => {
+    if (props.formBpm === true) {
+      if (props.formData.disabled === false) {
         return false;
-      }else{
+      } else {
         return true;
       }
     }
     return props.formDisabled;
   });
 
-  
   /**
    * 新增
    */
@@ -98,10 +113,10 @@
       resetFields();
       const tmpData = {};
       Object.keys(formData).forEach((key) => {
-        if(record.hasOwnProperty(key)){
-          tmpData[key] = record[key]
+        if (record.hasOwnProperty(key)) {
+          tmpData[key] = record[key];
         }
-      })
+      });
       //赋值
       Object.assign(formData, tmpData);
     });
@@ -154,7 +169,6 @@
         confirmLoading.value = false;
       });
   }
-
 
   defineExpose({
     add,
